2c9dc3378a6bbe71216065ce3fa2a797270a42ae
YaelUribe <yael3x@gmail.com>
2022-01-30 18:29:06 -0500

    new route, with default value of text

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,7 @@ def c_is_fun(text):
     return "C {}".format(text.replace('_', ' '))
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/(<text>)', strict_slashes=False)
 def python_is_cool(text='is cool'):
     return "Python {}".format(text.replace('_', ' '))
