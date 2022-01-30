8b202608f8755d154930cd9fe055d6e7ba652a8a
YaelUribe <yael3x@gmail.com>
2022-01-30 17:55:10 -0500

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Script: hello_route
+    starts a new Flask app"""
+from flask import Flask
+
+
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello_route():
+    return "Hello, HBNB!"
