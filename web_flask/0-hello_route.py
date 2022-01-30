09a48b3ece5d3aba2840ccfe7bc59fb391226107
YaelUribe <yael3x@gmail.com>
2022-01-30 18:04:36 -0500

    task 0, corrected output

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Script: hello_route
-    starts a new Flask app"""
+    starts a new Flask app
+"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_route():
-    return "Hello, HBNB!"
+    return "Hello HBNB!"
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
