7243fcd2dfb6e2abb7259a88d8771c0f4996fe6d
YaelUribe <yael3x@gmail.com>
2022-01-30 18:07:33 -0500

    Task1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script: hello_route
+    starts a new Flask app
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run()
